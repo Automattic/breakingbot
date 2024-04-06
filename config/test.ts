0bb28db66c52953c49f114adc285629b4f1c697d
Hew <github@mhs.fastmail.com>
2024-04-05 22:27:39 -0400

    Snip more

--- config/test.ts
+++ config/test.ts
@@ -1,7 +1,5 @@
-import type { AppConfig, SlackConfig, WpcomReporterConfig } from "./index.js";
+import type { AppConfig, SlackConfig } from "./index.js";
 import { priorityConfig } from "./priorities.js";
-
-const testBreakingRoomPrefix = "unit-test-breaking-";
 
 const testSlackConfig: SlackConfig = {
 	type: "Slack",
@@ -9,22 +7,12 @@ const testSlackConfig: SlackConfig = {
 	userIdRegexPattern: /(<@[UW][A-Z0-9]{5,19}>)|\b([UW][A-Z0-9]{5,19})\b/g,
 } as const;
 
-export const testWpcomConfig: WpcomReporterConfig = {
-	type: "Wpcom",
-	site: "unit-test.local",
-	postStatus: "draft",
-	chatRoomUrl: `${testSlackConfig.baseUrl}/messages`,
-	chatUserIdRegex: testSlackConfig.userIdRegexPattern,
-	trackerBaseUrl: "https://jira.unit-test.local/browse",
-} as const;
-
 export const testConfig: AppConfig = {
 	breakingInitialUsers: ["ryan", "nick", "sheri"],
 	breakingMainRoom: "unit-test-breaking-main",
 	breakingNotifyRoom: "unit-test-breaking-notify",
-	breakingRoomPrefix: testBreakingRoomPrefix,
+	breakingRoomPrefix: "unit-test-breaking-",
 	commPlatform: testSlackConfig,
 	priorities: priorityConfig,
-	reporterPlatform: testWpcomConfig,
 	runbookRootUrl: "https://automattic.com",
 } as const;
