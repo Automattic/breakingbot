94d5a622ad65c75f83dfa5bfd21a8ae77c35d561
Hew <2006764+mhsdef@users.noreply.github.com>
2024-04-08 17:12:07 -0400

    Add a8c deployments (#5)
    * Initial pass
    
    * Stub out wpcom reporter
    
    * Do this
    
    * Snip more
    
    * This too
    
    * improve
    
    * Add deployment workflow
    
    * Adjust
    
    * Fix
    
    * Better
    
    * Add prune
    
    * Tweak
    
    * Adjust comments
    
    * More tweak
    
    * Leh sigh

--- config/dev.ts
+++ config/dev.ts
@@ -3,13 +3,13 @@ import type { AppConfig, SlackConfig } from "./types.js";
 
 const devSlackConfig: SlackConfig = {
 	type: "Slack",
-	baseUrl: "https://<YOUR_INSTANCE>.slack.com",
+	baseUrl: "https://<INSTANCE>.slack.com",
 	userIdRegexPattern: /(<@[UW][A-Z0-9]{5,19}>)|\b([UW][A-Z0-9]{5,19})\b/g,
 } as const;
 
 export const devConfig: AppConfig = {
 	breakingInitialUsers: [],
-	breakingMainRoom: "breaking",
+	breakingMainRoom: "<SLACK_CHANNEL_ID>",
 	breakingRoomPrefix: "breaking-dev-",
 	commPlatform: devSlackConfig,
 	priorities: priorityConfig,
