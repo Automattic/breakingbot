043e374f68769dab901a41b3c4e7af2d0444921d
Hew <github@mhs.fastmail.com>
2024-04-08 11:56:25 -0400

    Adjust

--- config/types.ts
+++ config/types.ts
@@ -17,6 +17,7 @@ export type AppConfig = {
 	runbookRootUrl: string;
 	runbookPointUrl?: string;
 	runbookTriageUrl?: string;
+	skipDbSslCertCheck?: boolean;
 };
 
 export type CommPlatformConfig = SlackConfig;
