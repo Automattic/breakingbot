1d6a6c47ae1aad770391ab2bc6dbe748f7f2d146
Kata <kata@automattic.com>
2024-09-25 11:11:57 -0400

    revert to default aliases

--- config/priorities.ts
+++ config/priorities.ts
@@ -8,7 +8,7 @@ export const priorityConfig: PriorityConfig = {
 			emoji: "fire",
 			description:
 				"Critical issue that warrants public notification and liaison with executive teams, sending tweet, etc. The site is in a critical state and is actively impacting a large number of customers. Or we have an obviously critical security incident. All hands on deck! Highest communication cadence required.",
-			aliases: ["critical"],
+			aliases: ["hi", "high", "critical", "crit"],
 			nag: {
 				nagIntervalsSeconds: {
 					noComms: 120,
@@ -26,7 +26,7 @@ export const priorityConfig: PriorityConfig = {
 			emoji: "fire",
 			description:
 				"Something is seriously broken or degraded; but the blast radius is limited. Most breaking incidents fall into this category. _Serious_ security incidents involving _more than one_ customer fall into this category.",
-			aliases: ["high"],
+			aliases: ["mid", "med", "medium", "normal"],
 			nag: {
 				nagIntervalsSeconds: {
 					noComms: 1200,
@@ -44,7 +44,7 @@ export const priorityConfig: PriorityConfig = {
 			emoji: "dash",
 			description:
 				"Something is broken, or not fully working as intended, but it's not resulting in customer-facing errors or is impacting a singular or very small segment of customers. It's below “urgent”, but it needs to be fixed with high priority. Security incidents involving a _single_ customer fall here. Most plugin vulnerability incidents also belong here.",
-			aliases: ["medium"],
+			aliases: ["lo", "low", "lite", "light"],
 			nag: {
 				nagIntervalsSeconds: {
 					noComms: 1200,
@@ -58,13 +58,13 @@ export const priorityConfig: PriorityConfig = {
 			emoji: "heavy_multiplication_x",
 			description:
 				"Not a breaking incident. These are bugs or improvements that should be instead moved to sprint ticket work to be prioritized as time allows.",
-			aliases: ["minor"],
+			aliases: ["backlog", "none"],
 		},
 		5: {
 			name: "P5",
 			emoji: "heavy_multiplication_x",
 			description: "Not a breaking incident. Not an issue at all.",
-			aliases: ["low", "ticket", "client", "plan"],
+			aliases: ["wontfix"],
 		},
 	},
 } as const;
